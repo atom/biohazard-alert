57b5d774e363467fc7b7c6fb8d62cfacb8a096ad
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-03-20 16:17:38 -0700

    Extract sentiment analysis out into its own class

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,15 @@
 import { Application } from 'probot' // eslint-disable-line no-unused-vars
-import * as request from 'request-promise-native'
 
-const perspectiveKey = process.env.PERSPECTIVE_KEY
-const perspectiveApiUrl =
-  `https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze?key=${perspectiveKey}`
+import SentimentAnalyzer from './sentiment-analyzer'
 
 export = (app: Application) => {
   app.log.debug('Install issue_comment handler')
 
   app.on('issue_comment', async (context) => {
+    const analyzer = new SentimentAnalyzer(app.log)
     const commentUrl = context.payload.comment.html_url
     const commentText = context.payload.comment.body
-    const perspectiveRequest = {
-      url: perspectiveApiUrl,
-      body: {
-        comment: {
-          text: commentText
-        },
-        languages: ['en'],
-        requestedAttributes: {
-          TOXICITY: {}
-        }
-      },
-      json: true
-    }
-
-    app.log.debug(`Call Perspective API on ${commentUrl}`)
-    const perspectiveResponse = await (request.post(perspectiveRequest) as any)
-    app.log.debug(perspectiveResponse, `Perspective API response for ${commentUrl}`)
-
-    const score = perspectiveResponse.attributeScores.TOXICITY.summaryScore.value
+    const score = await analyzer.analyze(commentUrl, commentText)
 
     app.log.info(`Toxicity score ${score} for ${commentUrl}`)
   })
---
+++ src/sentiment-analyzer.ts
@@ -0,0 +1,36 @@
+import { Logger } from 'probot'
+import * as request from 'request-promise-native'
+
+export default class SentimentAnalyzer {
+  private apiUrl: string
+  private log: Logger
+
+  constructor(logger: Logger) {
+    const key = process.env.PERSPECTIVE_KEY
+
+    this.apiUrl = `https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze?key=${key}`
+    this.log = logger
+  }
+
+  async analyze(sourceUrl: string, content: string): Promise<number> {
+    const apiRequest = {
+      url: this.apiUrl,
+      body: {
+        comment: {
+          text: content
+        },
+        languages: ['en'],
+        requestedAttributes: {
+          TOXICITY: {}
+        }
+      },
+      json: true
+    }
+
+    this.log.debug(`Call Perspective API on ${sourceUrl}`)
+    const response = await (request.post(apiRequest) as any)
+    this.log.debug(response, `Perspective API response for ${sourceUrl}`)
+
+    return response.attributeScores.TOXICITY.summaryScore.value
+  }
+}
