7292a997463424091255c123445b9911deec6c4b
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-03-20 15:56:40 -0700

    Add very basic Perspective API infrastructure

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,36 @@
 import { Application } from 'probot' // eslint-disable-line no-unused-vars
+import * as request from 'request-promise-native'
+
+const perspectiveKey = process.env.PERSPECTIVE_KEY
+const perspectiveApiUrl =
+  `https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze?key=${perspectiveKey}`
 
 export = (app: Application) => {
+  app.log.debug('Install issue_comment handler')
+
   app.on('issue_comment', async (context) => {
+    const commentUrl = context.payload.comment.html_url
     const commentText = context.payload.comment.body
+    const perspectiveRequest = {
+      url: perspectiveApiUrl,
+      body: {
+        comment: {
+          text: commentText
+        },
+        languages: ['en'],
+        requestedAttributes: {
+          TOXICITY: {}
+        }
+      },
+      json: true
+    }
+
+    app.log.debug(`Call Perspective API on ${commentUrl}`)
+    const perspectiveResponse = await (request.post(perspectiveRequest) as any)
+    app.log.debug(perspectiveResponse, `Perspective API response for ${commentUrl}`)
+
+    const score = perspectiveResponse.attributeScores.TOXICITY.summaryScore.value
+
+    app.log.info(`Toxicity score ${score} for ${commentUrl}`)
   })
 }
