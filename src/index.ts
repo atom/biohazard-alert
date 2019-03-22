0e0ed20a7c5017d0dfaa1ff113c7417c925a634e
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-03-22 09:41:10 -0700

    Add handlers for other event types

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,18 @@ import Handler from './handler'
 export = (app: Application) => {
   const handler = new Handler(app.log)
 
-  app.log.debug('Install issue_comment handler')
+  app.log.debug('Install commit_comment handler')
+  app.on('commit_comment', async (context) => {
+    await handler.handle(context)
+  })
 
+  app.log.debug('Install issue_comment handler')
   app.on('issue_comment', async (context) => {
+    await handler.handle(context)
+  })
+
+  app.log.debug('Install issues handler')
+  app.on('issues', async (context) => {
     await handler.handle(context)
   })
 }
